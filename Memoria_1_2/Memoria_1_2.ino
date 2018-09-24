d0560e83f449ebc008daeb9d1f7673ce13075596
CarlosSanabriaM <uo250707@uniovi.es>
2018-09-25 00:22:38 +0200

    Añadidas funciones para dibujar la secuencia de colores.

--- Memoria_1_2/Memoria_1_2.ino
+++ Memoria_1_2/Memoria_1_2.ino
@@ -12,12 +12,12 @@ int colorSequence[10];
 // Indica el tamaño inicial de la secuencia
 int INITIAL_COLOR_SEQUENCE_SIZE = 3;
 // Indica el nº colores almacenados actualmente en el array anterior
-int colorSequenceSize = INITIAL_COLOR_SEQUENCE_SIZE; 
+int colorSequenceSize = INITIAL_COLOR_SEQUENCE_SIZE;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Setup");
-  
+
   // Entradas de los botones
   pinMode(redButton, INPUT);
   pinMode(greenButton, INPUT);
@@ -32,6 +32,33 @@ void setup() {
 void loop() {
   drawSequence(); // TODO
   checkSequence(); // TODO
+}
+
+void drawSequence() {
+  for (int i = 0; i < colorSequenceSize; i++) {
+    switchOnLed(colorSequence[i]);
+    delay(1000); // esperamos un seg entre cada encendido
+    switchOffLed(colorSequence[i]);
+  }
+}
+
+void switchOffLed(int ledColor) {
+  switchLed(ledColor, LOW);
+}
+
+void switchOnLed(int ledColor) {
+  switchLed(ledColor, HIGH);
+}
+
+void switchLed(int ledColor, int value) {
+  switch (ledColor) {
+    case 0: // rojo
+      digitalWrite(redLed, value);
+      break;
+    case 1: // verde
+      digitalWrite(greenLed, value);
+      break;
+  }
 }
 
 /* Devuelve un numero aleatorio: 0-rojo, 1-verde */
