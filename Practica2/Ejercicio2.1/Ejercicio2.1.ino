883052ce7fabe406297f0c2a9d6eb672a0390a34
CarlosSanabriaM <uo250707@uniovi.es>
2018-10-01 15:16:17 +0200

    Añadido esqueleto del Ejercicio2.1.

---
+++ Practica2/Ejercicio2.1/Ejercicio2.1.ino
@@ -0,0 +1,45 @@
+#include <Keypad.h>
+
+// Pines utilizados para los estados abierto y cerrado de la puerta
+const int ledDoorOpen = 6; // es un led rojo, que cuando está encendido indica puerta abierta/abriéndose
+const int ledDoorClosed = 7; // es un led verde, que cuando está encendido indica puerta cerrada/cerrándose
+
+// Teclado
+const byte numRows = 4; // numero de filas del teclado
+const byte numColumns = 4; // numero de columnas del teclado
+// Matriz con todas las teclas del teclado
+char keys[numRows][numColumns] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+byte rowsPins[numRows] = {2, 3, 4, 5}; // Pines utilizados para las filas
+byte columnsPins[numColumns] = {A0, A1, A2, A3}; // Pines utilizados para las columnas
+
+// Creamos un objeto Keypad que representa el teclado
+Keypad _keypad = Keypad(makeKeymap(keys), rowsPins, columnsPins, numRows, numColumns);
+
+// Buffer de lectura en el que iremos almacenando todas las teclas que pulsa el usuario
+String readingBuffer = "";
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("\n---------\n  Setup\n---------");
+
+  // Salidas de los leds
+  pinMode(ledDoorOpen, OUTPUT);
+  pinMode(ledDoorClosed, OUTPUT);
+}
+
+void loop() {
+  // Obtenemos la tecla pulsada (devuelve '\0' si no se pulsó ninguna)
+  char key = _keypad.getKey();
+
+  // Si se pulsó alguna tecla
+  if (key != '\0') {
+    Serial.println("Tecla pulsada: " + String(key));
+    
+  }
+
+}
