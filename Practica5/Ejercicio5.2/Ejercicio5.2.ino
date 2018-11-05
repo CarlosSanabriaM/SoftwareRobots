d62f796d312a65293a33e219f597ddb8ea46d3af
CarlosSanabriaM <uo250707@uniovi.es>
2018-11-05 14:52:28 +0100

    [Ejercicio5.2] Terminado.

--- Practica5/Ejercicio5.2/Ejercicio5.2.ino
+++ Practica5/Ejercicio5.2/Ejercicio5.2.ino
@@ -2,16 +2,19 @@
 
 // Variables para el servomotor del eje X
 Servo xAxisServo;
-int xAxisServoPin = 6;
+int xAxisServoPin = 7;
 // Variables para el servomotor del eje Y
 Servo yAxisServo;
-int yAxisServoPin = 7;
+int yAxisServoPin = 8;
+// Variables para el servomotor de la pinza
+Servo clampServo; // es un servomotor de 180º. No es de rotación continua.
+int clampServoPin = 4;
 
 // Variables para los servomotores de los ejes X e Y
-const int servoFastLeftOrUpValue = 0; // velocidad máxima de giro del servomotor en sentido antihorario
-const int servoSlowLeftOrUpValue = 80; // velocidad lenta de giro del servomotor en sentido antihorario
-const int servoFastRightOrDownValue = 180; // velocidad máxima de giro del servomotor en sentido horario
-const int servoSlowRightOrDownValue = 100; // velocidad lenta de giro del servomotor en sentido horario
+const int servoFastLeftOrUpValue = 180; // velocidad máxima de giro del servomotor en sentido antihorario
+const int servoSlowLeftOrUpValue = 100; // velocidad lenta de giro del servomotor en sentido antihorario
+const int servoFastRightOrDownValue = 0; // velocidad máxima de giro del servomotor en sentido horario
+const int servoSlowRightOrDownValue = 80; // velocidad lenta de giro del servomotor en sentido horario
 const int servoStopValue = 90; // valor en el que el servomotor no gira
 
 // Variables para el joystick 1 (controla los actuadores de los ejes X e Y)
@@ -37,6 +40,14 @@ const int Y_high_center_value = 525;
 const int Y_up_speed_change_point_value = 15; // entre 0 y 15 se considera que se quiere mover el actuador a máxima velocidad a la izda
 const int Y_down_speed_change_point_value = 1000; // entre 1000 y 1023 se considera que se quiere mover el actuador a máxima velocidad a la dcha
 
+// Variables para la pinza
+// Grados pinza, MAX y MIN admitido
+#define MAX_CLAMP_DEGREES 110
+#define MIN_CLAMP_DEGREES 10
+int clampDegrees = MIN_CLAMP_DEGREES; // almacena los grados actuales de la pinza
+// Entre 300 y 700 se considera que el joystick de la pinza está en el centro del eje x
+const int clamp_X_low_center_value = 300;
+const int clamp_X_high_center_value = 700;
 
 
 void setup() {
@@ -44,11 +55,16 @@ void setup() {
   Serial.println(F("\n---------\n  Setup\n---------"));
   xAxisServo.attach(xAxisServoPin);
   yAxisServo.attach(yAxisServoPin);
+  clampServo.attach(clampServoPin);
+
+  // Se inicializa la pinza
+  clampServo.write(clampDegrees);
 }
 
 void loop() {
   checkIfHaveToMoveXAxisActuator();
   checkIfHaveToMoveYAxisActuator();
+  checkIfHaveToOpenOrCloseTheClamp();
 }
 
 
@@ -90,20 +106,33 @@ void checkIfHaveToMoveYAxisActuator() {
   }
 }
 
+/* Comrprueba si hay que abrir o cerrar la pinza. */
+void checkIfHaveToOpenOrCloseTheClamp() {
+  // Obtenemos el valor del eje X del joystick 2
+  int XValue = getJoystickXValue(2);
+
+  // Si el valor de X es menor que el valor límite mínimo de la posicion central es que el joystick está hacia la izda
+  if (XValue < clamp_X_low_center_value) {
+    closeClamp();
+  }
+  // Si el valor de X es mayor que el valor límite máximo de la posicion central es que el joystick está hacia la dcha
+  else if (XValue > clamp_X_high_center_value) {
+    openClamp();
+  }
+}
+
 /* Devuelve el valor en el ejeX para el jostick indicado. */
 int getJoystickXValue(int jostickNum) {
   int XValue;
-  if(jostickNum == 1) XValue = analogRead(joystick1_X_pin);
+  if (jostickNum == 1) XValue = analogRead(joystick1_X_pin);
   else XValue = analogRead(joystick2_X_pin);
-  
-  Serial.println("X: " + String(XValue));
+
   return XValue;
 }
 
 /* Devuelve el valor en el ejeY para el jostick indicado. */
 int getJoystick1YValue() {
   int YValue = analogRead(joystick1_Y_pin);
-  Serial.println("Y: " + String(YValue));
   return YValue;
 }
 
@@ -157,7 +186,30 @@ void moveLinealActuatorDown(int YValue) {
 
 /* Detiene el actuador lineal del eje indicado. */
 void stopLinealActuator(char actuatorAxis) {
-  Serial.println("Se para actuador lineal del eje " + String(actuatorAxis));
-  if(actuatorAxis == 'X') xAxisServo.write(servoStopValue);
+  //Serial.println("Se para actuador lineal del eje " + String(actuatorAxis));
+  
+  if (actuatorAxis == 'X') xAxisServo.write(servoStopValue);
   else yAxisServo.write(servoStopValue);
+}
+
+/* Abre la pinza lentamente (se aumenta un grado al servomotor). */
+void openClamp() {
+  // Si los grados actuales de la pinza son menores que el máximo permitido
+  if(clampDegrees < MAX_CLAMP_DEGREES)
+    clampDegrees+=2; // incrementamos en 1 los grados de la pinza
+
+  Serial.println(F("Se abre la pinza"));
+  Serial.println("Grados pinza: " + String(clampDegrees));
+  clampServo.write(clampDegrees); // es de 180º. Al girar hasta los grados indicados se detiene.
+}
+
+/* Cierra la pinza lentamente (se aumenta un grado al servomotor). */
+void closeClamp() {
+  // Si los grados actuales de la pinza son mayores que el mínimo permitido
+  if(clampDegrees > MIN_CLAMP_DEGREES)
+    clampDegrees-=2; // decrementamos en 1 los grados de la pinza
+
+  Serial.println(F("Se cierra la pinza"));
+  Serial.println("Grados pinza: " + String(clampDegrees));
+  clampServo.write(clampDegrees); // es de 180º. Al girar hasta los grados indicados se detiene.
 }
