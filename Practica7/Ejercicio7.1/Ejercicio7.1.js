8af8e0d02908760627e362dfd12c367d1d75c800
CarlosSanabriaM <uo250707@uniovi.es>
2018-11-12 15:00:40 +0100

    [Ejercicio7.1] Parece que funciona el mapa.

--- Practica7/Ejercicio7.1/Ejercicio7.1.js
+++ Practica7/Ejercicio7.1/Ejercicio7.1.js
@@ -1,95 +1,81 @@
 "use strict";
-class MapaMeteorologico{
+class MapaSensores{
     constructor(){
-        this.mapa;
+        this.mapa; // el mapa de Google Maps
+        
         this.marcadores=[]; //Array para guardar los marcadores
         
-        this.apikeyOpenWeather = "7fc07660c3a999e7e1a9e6f53a925d92"; // TODO - quitar
-        this.unidades = "&units=metric"; // TODO - quitar
-        this.idioma = "&lang=es"; // TODO - quitar
-        
-        this.temperatura;
-        this.humedad;
-                
-        this.infowindow;
+        this.latitudArduino = 43.3545734; // latitud del arduino, harcodeada
+        this.longitudArduino = -5.8511042; // longitud del arduino, harcodeada
+        this.IPArduino = "192.168.61.37"; // ip del Arduino
     }
     
+    // Crea el mapa de Google y añade los marcadores correspondientes a los sensores
     inicializar() {
-        var estiloNocturno = this.getEstiloNocturno();//Creamos un estilo nocturno
+        // Inicializamos el mapa de Google
+        this.mapa = new google.maps.Map(document.getElementById('mapa-canvas'), {
+          center: {lat: this.latitudArduino, lng: this.longitudArduino},
+          zoom: 15
+        });
         
-        var mapaOpciones = {
-            zoom: 15,
-            mapTypeControlOptions: {
-                mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain', 'nocturno']
-            },
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        
-        this.mapa = new google.maps.Map(document.getElementById('mapa-canvas'), mapaOpciones);
-        this.mapa.mapTypes.set('nocturno', estiloNocturno);//Asociamos el 'typeId' nocturno con el estilo 'estiloNocturno'
-        this.mapa.setMapTypeId('nocturno');//Por defecto, le ponemos el modo nocturno
+        console.log(this);
+        console.log("MENSAJE");
 
-        this.addMarcadorConInfoWindow("Escuela informatica", 1111.11, 1111.11, 0, 0); // TODO - cambiar
-        this.addMarcadorConInfoWindow("Escuela informatica 2", 1112.11, 1112.11, 10, 10); // TODO - cambiar
+        // Añadimos los marcadores correspondientes a los sensores.
+        // Solo uno de ellos será un Arduino de verdad. El resto tienen valores fijos, que no cambian.
+        
+        // Añadimos el marcador con ventanita para el Arduino (los valores de temperatura y humedad 
+        // serán modificados la primera vez que se haga click en el marcador y se consulte al Arduino)
+        this.addMarcadorConInfoWindow("Escuela Ingeniería Informatica", this.latitudArduino, this.longitudArduino, 0, 0); 
+        // Añadimos el resto de marcadores con "sensores falsos"
+        this.addMarcadorConInfoWindow("Facultad de Magisterio", 43.357738, -5.855048, 20, 32);
+        this.addMarcadorConInfoWindow("Facultad de Ciencias ", 43.357936, -5.853565, 21, 30);
             
-        this.activarMarcadores(mapa);
+        //this.activarMarcadores(mapa); // TODO - quitar??
         //this.mapa.setCenter(this.posicionActual); // TODO - quitar
         //this.marcadorPosicionActual.setTitle("Tu posición"); // TODO - qutar
     }
     
-    //Activa todos los marcadores.
+    /*//Activa todos los marcadores. // TODO - quitar
     activarMarcadores(mapa) {
         for (var i = 0; i < this.marcadores.length; i++) {
             this.marcadores[i].setMap(mapa);
         }
-    }
+    }*/
 
-        
-    
-    cargarDatosMeteorologicos(latitud,longitud){
-        var urlArduino = "http://192.168.61.37/temperaturayhumedad";
+    // Consulta al Arduino la temperatura y la humedad utilizando Ajax con JQuery,
+    // y si tiene éxito, actualiza la temperatura y humedad en la web
+    cargarTemperaturaYHumedad(){
+        var urlArduino = "http://"+ this.IPArduino +"/temperaturayhumedad";
         
         $.ajax({
             dataType: "json",
             url: urlArduino,
             method: 'GET',
-            success: this.guardarTemperaturaYHumedad.bind(this),
+            success: this.actualizarTemperaturaYHumedad.bind(this),
             error:function(){
                 $("#mensajeError").html("¡En este momento no se puede obtener la información de la temperatura y la humedad!"); 
             }
         });
     }
     
-    guardarTemperaturaYHumedad(datos){
-        this.temperatura = datos.temperatura; // TODO - revisar
-        this.humedad = datos.humedad; // TODO - revisar
-    }
-    
-    mostrarDatosTiempo(){
-        $("#temperatura").html(this.temperatura);
-        $("#humedad").html(this.humedad);
+    // Actualiza en en el infowindow la temperatura y la humedad
+    actualizarTemperaturaYHumedad(datos){
+        $("#temperatura").html(datos.temperatura);
+        $("#humedad").html(datos.humedad);
     }
 
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    // Crea un marcador con las coordenadas indicadas y le añade un infowindow con la información indicada
     addMarcadorConInfoWindow(lugar, latitud, longitud, temperatura, humedad){
         var marcador = this.addMarcador(latitud, longitud, lugar);
         this.addInfoWindow(marcador, lugar, latitud, longitud, temperatura, humedad);
     }
     
-    //Añade un marcador y lo almacena en el array
+    // Crea un marcador con las coordenadas indicadas, le pone el title indicado en lugar,
+    // lo añade al mapa, y lo almacena en el array de marcadores
     addMarcador(latitud, longitud, lugar) {
         var marcador = new google.maps.Marker({
-            position: new google.maps.LatLng(latitud, longitud); //(-33.8665433,151.1956316);,
+            position: new google.maps.LatLng(latitud, longitud),
             map: this.mapa,
             title: lugar
         });
@@ -98,18 +84,19 @@ class MapaMeteorologico{
         return marcador;
     }
     
+    // Añade un infowindow al marcador indicado, y con la información indicada
     addInfoWindow(marcador, lugar, latitud, longitud, temperatura, humedad){      
         //Creamos una infoWindow con esta estructura html
         var contentString = '<div id="mensajeError"></div>'+
-                            '<div id="content">'+
-                                '<div id="cabecera">'+
+                            '<div>'+
+                                '<div>'+
                                     '<h2>'+ lugar +'</h2>'+
                                 '</div>'+
                                 '<div id="datos">'+
                                     '<p>Latitud: '+ latitud +'</p>'+
                                     '<p>Longitud: '+ longitud +'</p>'+
-                                    '<p>Temperatura: '+ temperatura +'</p>'+
-                                    '<p>Humedad: '+ humedad +'</p>'+
+                                    '<p>Temperatura:<span id="temperatura">'+ temperatura +'</span></p>'+
+                                    '<p>Humedad:<span id="humedad">'+ humedad +'</span></p>'+
                                 '</div>'+
                             '</div>';
 
@@ -117,101 +104,18 @@ class MapaMeteorologico{
           content: contentString
         });
 
-        marcador.setTitle(lugar);
+        //marcador.setTitle(lugar); // TODO - quitar??
         
+        // Añadimos un escuchador del evento click para cuando se pulse ese marcador
         marcador.addListener('click', this.clickInfoWindowHandler.bind(this,marcador,infowindow));
     }
     
+    // Cuando se hace click en un marcador, se hable su infoWindow correspondiente
     clickInfoWindowHandler(marcador,infowindow){
         infowindow.open(this.mapa, marcador);
     }
     
-    getEstiloNocturno(){
-        return new google.maps.StyledMapType(
-            [
-            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-            {
-              featureType: 'administrative.locality',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'geometry',
-              stylers: [{color: '#263c3f'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#6b9a76'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry',
-              stylers: [{color: '#38414e'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#212a37'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#9ca5b3'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry',
-              stylers: [{color: '#746855'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#1f2835'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#f3d19c'}]
-            },
-            {
-              featureType: 'transit',
-              elementType: 'geometry',
-              stylers: [{color: '#2f3948'}]
-            },
-            {
-              featureType: 'transit.station',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'geometry',
-              stylers: [{color: '#17263c'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#515c6d'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.stroke',
-              stylers: [{color: '#17263c'}]
-            }
-          ],
-            {name: 'Nocturno'});
-    }
-    
 }
 
-var mapa = new MapaMeteorologico();
+var mapa = new MapaSensores();
 google.maps.event.addDomListener(window, 'load', mapa.inicializar.bind(mapa));
