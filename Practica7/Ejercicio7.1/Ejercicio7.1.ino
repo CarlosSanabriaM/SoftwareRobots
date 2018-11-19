ae865fe2b6a82e9e16adb183cd315aabd3f18a69
CarlosSanabriaM <uo250707@uniovi.es>
2018-11-19 11:22:42 +0100

    [Ejercicio7.1] Terminado Ejercicio7.1.ino.

--- Practica7/Ejercicio7.1/Ejercicio7.1.ino
+++ Practica7/Ejercicio7.1/Ejercicio7.1.ino
@@ -1,5 +1,6 @@
-#include <SPI.h> //Importamos librería comunicación SPI
-#include <Ethernet.h> //Importamos librería Ethernet
+#include <SPI.h> // Librería comunicación SPI
+#include <Ethernet.h> // Librería Ethernet
+#include <DHT11.h> // Librería sensor temperatura y humedad
 
 byte mac[] = {0x54, 0x55, 0x58, 0x10, 0x00, 0x24};
 EthernetServer server(80); // Puerto en el 80
@@ -8,6 +9,10 @@ IPAddress gateway(192, 168, 61, 13); // Puerta de enlace predeterminada
 IPAddress subnet(255, 255, 255, 0); // Máscara de subred
 
 IPAddress ip(192, 168, 61, 37); // IP del Arduino
+
+// Variables para el sensor de temperatura y humedad
+int dht11Pin = 6;
+DHT11 dht11(dht11Pin);
 
 // Variables para la lógica
 int redLedPin = 8; // pin del led rojo
@@ -61,11 +66,9 @@ void loop() {
 /* La petición ha acabado, por lo que comprobamos su contenido y actuamos en consecuencia. */
 void petitionHasFinished(EthernetClient client, String petition) {
   if (contains(petition, TEMPERATURE_AND_HUMIDITY_ENDPOINT)) {
-    // Primero enviamos la cabecera de la respuesta HTTP y luego los datos en JSON.
-    sendClient_HTTP_OK_Response(client);
-    sendClientTemperatureAndHumidity(client);
+    sendClientTemperatureAndHumidityAnd_HTTP_Response(client);
   }
-  else if(contains(petition, SWITCH_ON_LED_ENDPOINT)){
+  else if (contains(petition, SWITCH_ON_LED_ENDPOINT)) {
     switchOnLed();
     sendClient_HTTP_OK_Response(client);
   }
@@ -75,17 +78,55 @@ void petitionHasFinished(EthernetClient client, String petition) {
   }
 }
 
-/* Envia al cliente los datos de temperatura y humedad. */
-void sendClientTemperatureAndHumidity(EthernetClient client){
-  client.println("{\"temperatura\":" + String(temperature) + ", \"humedad\":" + String(humidity) + "}"); // TODO - temperature y humidity como vars globales
+/*
+  Obtiene los datos de temperatura y humedad.
+  Si los obtiene correctamente, los envia al cliente junto a una respuesta OK.
+  Si no, devuelve una respuesta 500 Internal Server Error.
+*/
+void sendClientTemperatureAndHumidityAnd_HTTP_Response(EthernetClient client) {
+  // Obtenemos la temperatura y la humedad del sensor
+  float temperature, humidity;
+  int exitCode = dht11.read(humidity, temperature);
+
+  // Si el código de salida es 0, es que obtuvo los datos correctamente
+  if (exitCode == 0)
+    temperatureAndHumidityCorrectlyObtained(client, temperature, humidity);
+  // Si no es 0, es que hubo un error
+  else
+    cantObtainTemperatureAndHumidity(client, exitCode);
+}
+
+/* Envia al cliente una respuesta 200 OK, y la temperatura y la humedad en formato JSON. */
+void temperatureAndHumidityCorrectlyObtained(EthernetClient client, float temperature, float humidity) {
+  Serial.println("Temperatura: " + String(temperature));
+  Serial.println("Humedad: " + String(humidity));
+  Serial.println(F("--------------------------------"));
+
+  sendClient_HTTP_OK_Response(client);  
+  client.println("{\"temperatura\":" + String(temperature) + ", \"humedad\":" + String(humidity) + "}");
   client.println();
 }
 
+/* Imprime el error por consola y envia al cliente una respuesta 500 Internal Server Error. */
+void cantObtainTemperatureAndHumidity(EthernetClient client, int exitCode) {
+  Serial.println("Error: " + String(exitCode));
+  Serial.println(F("--------------------------------"));
+
+  sendClient_HTTP_500_Error_Response(client);
+}
+
 /* Envia al cliente una respuesta HTTP 200 OK. */
-void sendClient_HTTP_OK_Response(EthernetClient client){
-  // Enviamos al cliente una respuesta HTTP 200 OK
+void sendClient_HTTP_OK_Response(EthernetClient client) {
   client.println("HTTP/1.1 200 OK");
-  // client.println("Content-Type: text/html"); // TODO
+  client.println("Content-Type: application/json;charset=utf-8");
+  client.println("Server: Arduino");
+  client.println("Access-Control-Allow-Origin: *");
+  client.println();
+}
+
+/* Envia al cliente una respuesta HTTP 500 Internal Server Error. */
+void sendClient_HTTP_500_Error_Response(EthernetClient client) {
+  client.println("HTTP/1.1 500");
   client.println("Content-Type: application/json;charset=utf-8");
   client.println("Server: Arduino");
   client.println("Access-Control-Allow-Origin: *");
